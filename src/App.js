c80bd6d46f5a6873b5c919991dc2ffde03d9e4b4
khushi-omar <khushi007omar@email.com>
2025-05-14 15:55:13 +0530

    new layout

--- src/App.js
+++ src/App.js
@@ -1,104 +1,9 @@
-/*import { useState, useEffect } from "react";
-import './App.css';
-import SearchIcon from "./search.svg";
-import MovieCard from "./MovieCard";
-import { FaHome, FaFilm, FaLaugh, FaRobot, FaClock, FaInfoCircle } from 'react-icons/fa';
 
-//const API_URL = "http://www.omdbapi.com?apikey=b169cf0";
-const API_URL = "https://www.omdbapi.com?apikey=b169cf0";
-
-function App() {
-  const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const searchMovies = async (title) => {
-    const totalPages = 3;
-    let allMovies = [];
-
-    for (let page = 1; page <= totalPages; page++) {
-      const response = await fetch(`${API_URL}&s=${title}&page=${page}`);
-      const data = await response.json();
-
-      if (data.Search) {
-        allMovies = [...allMovies, ...data.Search];
-      }
-    }
-
-    setMovies(allMovies);
-  };
-
-  useEffect(() => {
-    searchMovies("Batman");
-  }, []);
-
-  return (
-    <div className="app-container"> 
-    <div className="sidebar">
-  <div className="logo-section">
-    <h2>🎬 MovieGo</h2>
-    <p> Stay Hooked !!</p>
-  </div>
-  <ul className="nav-menu">
-    <li onClick={() => searchMovies("Batman")}><FaHome/>  Home</li>
-    <li onClick={() => searchMovies("Action")}><FaFilm/>  Action</li>
-    <li onClick={() => searchMovies("Comedy")}><FaLaugh/>  Comedy</li>
-    <li onClick={() => searchMovies("Sci-Fi")}><FaRobot/>Sci-Fi</li>
-    <li onClick={() => searchMovies("2023")}><FaClock/>Latest</li>
-    <li onClick={() => alert("Built by Khushi")}><FaInfoCircle/>About</li>
-  </ul>
-</div>
-
-
-      
-      <div className="main-content">
-        <header className="app-header">
-          <h1>🎬 MovieHUB</h1>
-          <p>Your go-to place for everything movies!</p>
-        </header>
-
-        <div className="search-bar">
-  <input
-    type="text"
-    placeholder="Search for movies..."
-    value={searchTerm}
-    onChange={(e) => setSearchTerm(e.target.value)}
-    onKeyDown={(e) => {
-      if (e.key === "Enter") {
-        searchMovies(searchTerm);
-      }
-    }}
-  />
-  <button onClick={() => searchMovies(searchTerm)}>
-    <img src={SearchIcon} alt="Search" />
-  </button>
-</div>
-
-
-        {movies?.length > 0 ? (
-          <div className="container">
-            {movies.map((movie, index) => (
-              <MovieCard key={index} movie={movie} />
-            ))}
-          </div>
-        ) : (
-          <div className="empty">
-            <h2>No Movies Found</h2>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-}
-
-export default App;
-*/
-
-// App.js
 import { useState, useEffect } from "react";
 import './App.css';
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
-import { FaHome, FaFilm, FaLaugh, FaRobot, FaClock, FaInfoCircle } from 'react-icons/fa';
+import { FaHome, FaFilm, FaLaugh, FaRobot, FaClock, FaInfoCircle, FaPoll, FaHeart } from 'react-icons/fa';
 
 const API_URL = "https://www.omdbapi.com?apikey=b169cf0";
 
@@ -138,6 +43,8 @@ function App() {
           <li onClick={() => searchMovies("Action")}><FaFilm/> Action</li>
           <li onClick={() => searchMovies("Comedy")}><FaLaugh/> Comedy</li>
           <li onClick={() => searchMovies("Sci-Fi")}><FaRobot/> Sci-Fi</li>
+          <li onClick={() => searchMovies("Sci-Fi")}><FaHeart/> Popular</li>
+          <li onClick={() => searchMovies("Sci-Fi")}><FaRobot/> Favourites</li>
           <li onClick={() => searchMovies("2023")}><FaClock/> Latest</li>
           <li onClick={() => alert("Built by Khushi")}><FaInfoCircle/> About</li>
         </ul>
@@ -183,3 +90,4 @@ function App() {
 }
 
 export default App;
+
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,39 +1,6 @@
-/*import React from 'react';
-import './MovieCard.css';
-
-const MovieCard = ({ movie }) => {
-  return (
-    <div className="movie-card">
-        
-      <div className="poster-wrapper">
-       
-        <img
-          src={
-            movie.Poster !== 'N/A'
-              ? movie.Poster
-              : 'https://via.placeholder.com/200x300?text=No+Image'
-          }
-          alt={movie.Title}
-          className="poster"
-        />
-        <div className="overlay">
-          <div className="movie-info">
-            <h3>{movie.Title}</h3>
-            <p>{movie.Year} · {movie.Type.toUpperCase()}</p>
-            <button className="details-button">See Details</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default MovieCard;
-
-*/
-// MovieCard.js
 import React from 'react';
 import './MovieCard.css';
+import { FaInfo } from 'react-icons/fa';
 
 const MovieCard = ({ movie }) => {
   return (
@@ -52,7 +19,7 @@ const MovieCard = ({ movie }) => {
           <div className="movie-info">
             <h3>{movie.Title}</h3>
             <p>{movie.Year} · {movie.Type.toUpperCase()}</p>
-            <button className="details-button">See Details</button>
+            <button className="details-button"><FaInfo/>See Details</button>
           </div>
         </div>
       </div>
